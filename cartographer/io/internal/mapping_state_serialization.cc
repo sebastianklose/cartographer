42739ec28e3f38806248582ec528780cf8be8e10
Sebastian Klose <klose@google.com>
2018-05-25 11:21:22 +0200

    updating interfaces

--- cartographer/io/internal/mapping_state_serialization.cc
+++ cartographer/io/internal/mapping_state_serialization.cc
@@ -23,6 +23,9 @@ namespace io {
 
 namespace {
 
+// The current serialization format version.
+constexpr int kSerializationFormatVersion = 1;
+
 mapping::proto::AllTrajectoryBuilderOptions
 CreateAllTrajectoryBuilderOptionsProto(
     const std::vector<mapping::proto::TrajectoryBuilderOptionsWithSensorIds>&
@@ -36,6 +39,12 @@ CreateAllTrajectoryBuilderOptionsProto(
   return all_options_proto;
 }
 
+mapping::proto::SerializationHeader CreateHeader() {
+  mapping::proto::SerializationHeader header;
+  header.set_version(kSerializationFormatVersion);
+  return header;
+}
+
 }  // namespace
 
 void ToPbStream(
@@ -43,85 +52,41 @@ void ToPbStream(
     const std::vector<mapping::proto::TrajectoryBuilderOptionsWithSensorIds>&
         trajectory_builder_options,
     ProtoStreamWriterInterface* const writer) {
+  writer->WriteProto(CreateHeader());
+
+  mapping::proto::SerializedData proto;
   // We serialize the pose graph followed by all the data referenced in it.
-  writer->WriteProto(pose_graph.ToProto());
-  writer->WriteProto(
-      CreateAllTrajectoryBuilderOptionsProto(trajectory_builder_options));
-  {
-    for (const auto& submap_id_data : pose_graph.GetAllSubmapData()) {
-      mapping::proto::SerializedData proto;
-      auto* const submap_proto = proto.mutable_submap();
-      submap_proto->mutable_submap_id()->set_trajectory_id(
-          submap_id_data.id.trajectory_id);
-      submap_proto->mutable_submap_id()->set_submap_index(
-          submap_id_data.id.submap_index);
-      submap_id_data.data.submap->ToProto(
-          submap_proto, /*include_probability_grid_data=*/true);
-      writer->WriteProto(proto);
-    }
+  *proto.mutable_pose_graph() = pose_graph.ToProto();
+  writer->WriteProto(proto);
+  *proto.mutable_all_trajectory_builder_options() =
+      CreateAllTrajectoryBuilderOptionsProto(trajectory_builder_options);
+  writer->WriteProto(proto);
+
+  // Next serialize all submaps.
+  for (const auto& submap_id_data : pose_graph.GetAllSubmapData()) {
+    auto* const submap_proto = proto.mutable_submap();
+    submap_proto->mutable_submap_id()->set_trajectory_id(
+        submap_id_data.id.trajectory_id);
+    submap_proto->mutable_submap_id()->set_submap_index(
+        submap_id_data.id.submap_index);
+    submap_id_data.data.submap->ToProto(submap_proto,
+                                        /*include_probability_grid_data=*/true);
+    writer->WriteProto(proto);
   }
   // Next we serialize all node data.
-  {
-    for (const auto& node_id_data : pose_graph.GetTrajectoryNodes()) {
-      mapping::proto::SerializedData proto;
-      auto* const node_proto = proto.mutable_node();
-      node_proto->mutable_node_id()->set_trajectory_id(
-          node_id_data.id.trajectory_id);
-      node_proto->mutable_node_id()->set_node_index(node_id_data.id.node_index);
-      *node_proto->mutable_node_data() =
-          ToProto(*node_id_data.data.constant_data);
-      writer->WriteProto(proto);
-    }
-  }
-  // Next we serialize IMU data from the pose graph.
-  {
-    const auto all_imu_data = pose_graph.GetImuData();
-    for (const int trajectory_id : all_imu_data.trajectory_ids()) {
-      for (const auto& imu_data : all_imu_data.trajectory(trajectory_id)) {
-        mapping::proto::SerializedData proto;
-        auto* const imu_data_proto = proto.mutable_imu_data();
-        imu_data_proto->set_trajectory_id(trajectory_id);
-        *imu_data_proto->mutable_imu_data() = sensor::ToProto(imu_data);
-        writer->WriteProto(proto);
-      }
-    }
-  }
-  // Next we serialize odometry data from the pose graph.
-  {
-    const auto all_odometry_data = pose_graph.GetOdometryData();
-    for (const int trajectory_id : all_odometry_data.trajectory_ids()) {
-      for (const auto& odometry_data :
-           all_odometry_data.trajectory(trajectory_id)) {
-        mapping::proto::SerializedData proto;
-        auto* const odometry_data_proto = proto.mutable_odometry_data();
-        odometry_data_proto->set_trajectory_id(trajectory_id);
-        *odometry_data_proto->mutable_odometry_data() =
-            sensor::ToProto(odometry_data);
-        writer->WriteProto(proto);
-      }
-    }
-  }
-  // Next we serialize all fixed frame pose data from the pose graph.
-  {
-    const auto all_fixed_frame_pose_data = pose_graph.GetFixedFramePoseData();
-    for (const int trajectory_id : all_fixed_frame_pose_data.trajectory_ids()) {
-      for (const auto& fixed_frame_pose_data :
-           all_fixed_frame_pose_data.trajectory(trajectory_id)) {
-        mapping::proto::SerializedData proto;
-        auto* const fixed_frame_pose_data_proto =
-            proto.mutable_fixed_frame_pose_data();
-        fixed_frame_pose_data_proto->set_trajectory_id(trajectory_id);
-        *fixed_frame_pose_data_proto->mutable_fixed_frame_pose_data() =
-            sensor::ToProto(fixed_frame_pose_data);
-        writer->WriteProto(proto);
-      }
-    }
+  for (const auto& node_id_data : pose_graph.GetTrajectoryNodes()) {
+    auto* const node_proto = proto.mutable_node();
+    node_proto->mutable_node_id()->set_trajectory_id(
+        node_id_data.id.trajectory_id);
+    node_proto->mutable_node_id()->set_node_index(node_id_data.id.node_index);
+    *node_proto->mutable_node_data() =
+        ToProto(*node_id_data.data.constant_data);
+    writer->WriteProto(proto);
   }
   // Next we serialize all trajectory data.
   {
     const auto all_trajectory_data = pose_graph.GetTrajectoryData();
     for (const auto& trajectory_data : all_trajectory_data) {
-      mapping::proto::SerializedData proto;
       auto* const trajectory_data_proto = proto.mutable_trajectory_data();
       trajectory_data_proto->set_trajectory_id(trajectory_data.first);
       trajectory_data_proto->set_gravity_constant(
@@ -139,6 +104,47 @@ void ToPbStream(
       writer->WriteProto(proto);
     }
   }
+  // Next we serialize IMU data from the pose graph.
+  {
+    const auto all_imu_data = pose_graph.GetImuData();
+    for (const int trajectory_id : all_imu_data.trajectory_ids()) {
+      for (const auto& imu_data : all_imu_data.trajectory(trajectory_id)) {
+        auto* const imu_data_proto = proto.mutable_imu_data();
+        imu_data_proto->set_trajectory_id(trajectory_id);
+        *imu_data_proto->mutable_imu_data() = sensor::ToProto(imu_data);
+        writer->WriteProto(proto);
+      }
+    }
+  }
+  // Next we serialize odometry data from the pose graph.
+  {
+    const auto all_odometry_data = pose_graph.GetOdometryData();
+    for (const int trajectory_id : all_odometry_data.trajectory_ids()) {
+      for (const auto& odometry_data :
+           all_odometry_data.trajectory(trajectory_id)) {
+        auto* const odometry_data_proto = proto.mutable_odometry_data();
+        odometry_data_proto->set_trajectory_id(trajectory_id);
+        *odometry_data_proto->mutable_odometry_data() =
+            sensor::ToProto(odometry_data);
+        writer->WriteProto(proto);
+      }
+    }
+  }
+  // Next we serialize all fixed frame pose data from the pose graph.
+  {
+    const auto all_fixed_frame_pose_data = pose_graph.GetFixedFramePoseData();
+    for (const int trajectory_id : all_fixed_frame_pose_data.trajectory_ids()) {
+      for (const auto& fixed_frame_pose_data :
+           all_fixed_frame_pose_data.trajectory(trajectory_id)) {
+        auto* const fixed_frame_pose_data_proto =
+            proto.mutable_fixed_frame_pose_data();
+        fixed_frame_pose_data_proto->set_trajectory_id(trajectory_id);
+        *fixed_frame_pose_data_proto->mutable_fixed_frame_pose_data() =
+            sensor::ToProto(fixed_frame_pose_data);
+        writer->WriteProto(proto);
+      }
+    }
+  }
   // Next we serialize all landmark data.
   {
     const std::map<std::string /* landmark ID */,
@@ -146,7 +152,6 @@ void ToPbStream(
         all_landmark_nodes = pose_graph.GetLandmarkNodes();
     for (const auto& node : all_landmark_nodes) {
       for (const auto& observation : node.second.landmark_observations) {
-        mapping::proto::SerializedData proto;
         auto* landmark_data_proto = proto.mutable_landmark_data();
         landmark_data_proto->set_trajectory_id(observation.trajectory_id);
         landmark_data_proto->mutable_landmark_data()->set_timestamp(
